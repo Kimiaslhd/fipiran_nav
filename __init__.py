658268e3f35c14e3e38a80113075b7e2bc0cdf1c
Kimia Salehi Delarestaghi <113443561+Kimiaslhd@users.noreply.github.com>
2025-08-31 09:03:41 +0330

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
-__version__ = "1.0.0"
+__version__ = "1.0.2"
 
 from .csv_export import fetch_csv
 from .core import fetch_date_range, fetch_api_data, get_all_dates
+
 
 __all__ = ['fetch_csv', 'fetch_date_range', 'fetch_api_data', 'get_all_dates']
